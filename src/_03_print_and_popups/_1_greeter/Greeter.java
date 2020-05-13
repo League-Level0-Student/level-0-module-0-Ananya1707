c73f568793021cd230a936c62aa53eb1eb55f9f0
root <root@b08ae789554b>
2020-05-13 00:20:55 +0000

    World Domination

--- src/_03_print_and_popups/_1_greeter/Greeter.java
+++ src/_03_print_and_popups/_1_greeter/Greeter.java
@@ -1,5 +1,11 @@
 package _03_print_and_popups._1_greeter;
 
-public class Greeter {
+import javax.swing.JOptionPane;
 
+public class Greeter {
+public static void main(String[] args) {
+	System.out.println("Hello World!");
+	String input = JOptionPane.showInputDialog("What is your name?");
+	JOptionPane.showMessageDialog(null,"Hello "+input);
+}
 }
